efc15e0838bc14f01146e9664ef8129c2ea70655
Geovani Cardenas <83044626+MisaelCardenas@users.noreply.github.com>
2022-12-01 02:20:33 -0600

    Add files via upload

---
+++ Proyect_Estacionamiento/app/src/main/java/com/example/proyect_estacionamiento/Espacios.kt
@@ -0,0 +1,21 @@
+package com.example.proyect_estacionamiento
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.proyect_estacionamiento.databinding.ActivityEspaciosBinding
+
+
+class Espacios : AppCompatActivity() {
+    //vincular la vista
+    private lateinit var binding: ActivityEspaciosBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityEspaciosBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textV.text
+
+    }
+}
